90258f8d665ca6e64646428c25fa4763cdb7b79e
Nikolai Kotula <gkotula@salesforce.com>
2024-05-30 10:24:19 -0600

    Gets basic example written

--- app/src/main/java/com/salesforce/gkotula/MainActivity.kt
+++ app/src/main/java/com/salesforce/gkotula/MainActivity.kt
@@ -1,20 +1,117 @@
 package com.salesforce.gkotula
 
 import android.os.Bundle
+import android.util.Log
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.io.InputStream
+import java.util.concurrent.atomic.AtomicBoolean
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.my_web_view)!!
+        webView.clearCache(true)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = buildWebViewClientBlockingResponse(sleepTime = 30_000L)
+//        webView.webViewClient = buildWebViewClientBlockingRead(sleepTime = 30_000L)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        webView.loadUrl(START_URL)
+    }
+
+    private fun buildWebViewClientBlockingResponse(sleepTime: Long) = object : WebViewClient() {
+        override fun shouldInterceptRequest(
+            view: WebView?,
+            request: WebResourceRequest?
+        ): WebResourceResponse? {
+            val url = request?.url?.toString() ?: return null
+            Log.d(TAG, "shouldInterceptRequest blocking response: $url")
+
+            return when {
+                url.startsWith(START_URL) -> startingHtmlResponse()
+
+                url.startsWith(FOO_URL) -> {
+                    Thread.sleep(sleepTime)
+                    return WebResourceResponse(
+                        "text/javascript",
+                        "utf-8",
+                        SCRIPT_CONTENT.byteInputStream()
+                    )
+                }
+
+                else -> super.shouldInterceptRequest(view, request)
+            }
+        }
+    }
+
+    private fun buildWebViewClientBlockingRead(sleepTime: Long) = object : WebViewClient() {
+        override fun shouldInterceptRequest(
+            view: WebView?,
+            request: WebResourceRequest?
+        ): WebResourceResponse? {
+            val url = request?.url?.toString() ?: return null
+            Log.d(TAG, "shouldInterceptRequest blocking read: $url")
+
+            return when {
+                url.startsWith(START_URL) -> startingHtmlResponse()
+
+                url.startsWith(FOO_URL) -> {
+                    val returnStream = object : InputStream() {
+                        private val targetStream = SCRIPT_CONTENT.byteInputStream()
+                        private val isFirstRead = AtomicBoolean(true)
+                        override fun read(): Int {
+                            if (isFirstRead.compareAndSet(true, false)) {
+                                Log.d(TAG, "shouldInterceptRequest blocking read first time: $url")
+                                Thread.sleep(sleepTime)
+                            }
+                            return targetStream.read()
+                        }
+                    }
+                    WebResourceResponse("text/javascript", "utf-8", returnStream)
+                }
+
+                else -> super.shouldInterceptRequest(view, request)
+            }
+        }
+    }
+
+    private fun startingHtmlResponse() =
+        WebResourceResponse(
+            "text/html",
+            "utf-8",
+            BASIC_HTML.byteInputStream()
+        )
+
+    companion object {
+        private const val FOO_URL = "https://foo"
+        private const val START_URL = "https://nkotula-start"
+        private val BASIC_HTML = """
+            <!DOCTYPE html>
+            <html>
+              <head></head>
+              <body>Hello, World!</body>
+            </html>
+        """.trimIndent()
+        private const val SCRIPT_CONTENT = "console.log('Script loaded');"
+        private const val TAG = "NKotula"
     }
 }
